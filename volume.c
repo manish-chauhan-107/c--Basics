1c9cb46fac8875d2fedc81f427980249610a0dc1
Manish chauhan <nitm.manishchauhan@gmail.com>
2025-09-06 19:39:52 +0530

    Create volume.c
    volume of a cube and use of associativity rule

---
+++ volume.c
@@ -0,0 +1,17 @@
+//volume of a sphere 
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+
+    float radius ;
+    printf("enter radius ");
+    scanf("%f", &radius);
+
+    float pi = 3.14;
+
+    //operator use '*'and '/' both has same priority. so, associativity rule work here 
+
+    printf("volume of sphere : %.2f" , 4.0 / 3 * pi * pow(radius, 3));
+    return 0;
+}
