e7e0efe680e495f5367b275400a278a84af77e71
Manish chauhan <nitm.manishchauhan@gmail.com>
2025-09-07 15:13:48 +0530

    even_odd_number_check.c
    Conditional statements (if, else)

---
+++ even_odd_number_check.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+   int number;
+    printf("enter number : ");
+    scanf("%d" , &number );
+   if (number % 2 == 0){
+        printf("This is an even Number. \n ");
+    }
+    else {
+        printf("This is an odd number. \n ");
+    }
+ /* In place of block(7-12) we can use Ternary operators.That is 
+'number % 2 == 0 ? printf("This is an even number "):printf("this is a odd number ");'*/
+    return 0;
+    }
