5e413407d7789281ec6a82c2821ac2bc1e21bf97
Manish chauhan <nitm.manishchauhan@gmail.com>
2025-09-13 21:45:55 +0530

    integer-classifier.c
    This C program takes an integer input from the user and checks whether the number is:
    
    - Positive or Negative  
    - Even or Odd  
    - Or Zero (neither positive nor negative)
    
    It demonstrates the use of:
    - if-else conditional statements
    - Modulus operator % for even/odd check
    - Basic input/output functions (scanf , printf)

---
+++ integer-classifier.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+   int number;
+   printf("Enter number :");
+   scanf("%d" , &number );
+
+   if (number > 0) 
+   {
+     if (number % 2 == 0)
+     {
+       printf (" Number is positive \n Number is Even");
+     }
+     else
+      {
+         printf (" Number is positive \n Number is odd");
+      }
+    }
+   else if (number < 0 )
+   {
+      if ( number % 2 == 0 )
+        {
+          printf(" Number is negative \n Number is even");
+        }
+      else 
+        {
+            printf (" Number is negative \n Number is odd");
+        }
+    } 
+   else {
+    printf ("Number is nither positive nor negative ");
+   }
+    return 0;
+}
