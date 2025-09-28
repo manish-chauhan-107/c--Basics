91b6234516536ccb289009c8b9ea3e3bd4d36543
Manish chauhan <nitm.manishchauhan@gmail.com>
2025-09-28 15:30:45 +0530

    factorial_finder.c
    A simple C program to calculate the factorial of a given number using a for loop.

---
+++ factorial_calculator.c
@@ -0,0 +1,14 @@
+//factorial of number
+#include<stdio.h>
+int main (){
+   int a;
+   int multiply = 1;
+    printf("Enter number: ");
+    scanf("%d" , &a);
+    for (int i = a; i>0 ; i = i - 1)
+    {
+        multiply = multiply*i;
+    }
+     printf("%d! = %d " ,a ,multiply );
+    return 0 ;
+}
