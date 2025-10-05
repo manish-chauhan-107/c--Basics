2e8cf6273f798af4f8f4098cdfde76e1ffbee2b5
Manish chauhan <nitm.manishchauhan@gmail.com>
2025-10-05 18:58:10 +0530

    area_of_square.c
    A simple C program to calculate the area of a square. The user enters the length of side of the square, and the program calculates the area using the formula:
                    Area = (side)^2
    The calculation is done using the pow() function from the <math.h> library, and the result is displayed up to two decimal places. This program demonstrates the use of math functions, input handling, and formatted output in C.

---
+++ area_of_square.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+#include<math.h>
+int main ()
+{
+    int a ;
+    printf("Enter side of square: ");
+    scanf("%d" , &a );
+    
+     printf("Answer = %.2f." , pow(a , 2));
+    
+    return 0;
+}
