803da0a6ace76e2245d528a7045ddfa3dfd3cae3
Manish chauhan <nitm.manishchauhan@gmail.com>
2025-10-06 19:51:52 +0530

    triangle_pattern.c
    A simple C program that prints a right-angled triangle star pattern using nested loops. Perfect for beginners learning about loops in C.

--- triangle_pattern.c
+++ triangle_pattern.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 int main ()
 {
-    for(int i = 1 ; i <= 5 ; i++)
+    int a;
+    printf("Enter a number: ");
+    scanf("%d" , &a);
+    for(int i = 1 ; i <= a ; i++)
     {
         for(int j = 1 ; j<=i ; j++)
         {
