f449b976307d589496b25391925c03dbe17e9d30
Manish chauhan <nitm.manishchauhan@gmail.com>
2025-10-08 23:23:22 +0530

    reverse_right_angle_triangle
    A simple C program that prints an inverted right-angled triangle pattern using the number 1. The user enters the number of rows, and the program displays the pattern with decreasing numbers of 1s in each row. This program demonstrates the use of nested loops and pattern printing logic in C.

--- star_pattern.c
+++ star_pattern.c
@@ -6,9 +6,9 @@ int main ()
     scanf("%d" , &a);
     for(int i = 1 ; i <= a ; i++)
     {
-        for(int j = 1 ; j<=i ; j++)
+        for(int j = a ; j>i ; j--)
         {
-            printf("* ");
+            printf("1 ");
         }
         printf("\n");
     }
