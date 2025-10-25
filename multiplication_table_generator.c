285719bd4c6e9854e649ab8ebc0d85af0f9d619b
Manish chauhan <nitm.manishchauhan@gmail.com>
2025-10-25 19:20:58 +0530

    Update multiplication_table_generator.c
    A simple C program that generates and displays the multiplication table of a given number up to 10.

--- multiplication_table_generator.c
+++ multiplication_table_generator.c
@@ -7,7 +7,7 @@ int main ()
     scanf("%d",&a);
     for (i=1 ; i<=10 ; i++)
     {
-        printf ("%d x %d = %d \n" , a , i , a*i );
+        printf ("%d x %d = %d \n" , a , i , a*i );     // [2 x 5 = 10] table print like this 
     }
    
     return 0 ;
