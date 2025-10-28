920ee9b7073e38df1deae25c67759abfda28b871
Manish chauhan <nitm.manishchauhan@gmail.com>
2025-10-28 23:33:52 +0530

    Implement array merging functionality
    Merge two arrays a and b into array c and print the result.

---
+++ array_merge.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main (){
+    int a[5] = {1,2,3,4,5};
+    int b[6] = {6,7,8,9,10,11};
+    int c[11];
+    int i , j ;
+    for (i=0 ; i<5 ; i++ )
+    {
+        c[i] = a[i];
+    }
+    for (j=0 ; j<6 ; j++ )
+    {
+        c[j] = a[j];
+    }
+    printf("array Merged:");
+    for (int k = 0; k < 11; k++)
+    {
+        printf("%d ", c[k]);
+    }
+    return 0;
+}
