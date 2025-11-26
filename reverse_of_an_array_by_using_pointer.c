5c41143736692ad549b38c423b332fd9736141fc
Manish Chauhan <nitm.manishchauhan@gmail.com>
2025-11-26 22:24:46 +0530

    reverse of an array by using pointer

---
+++ reverse_of_an_array_by_using_pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+  int array[10] = {1,2,3,4,5,6,7,8,9,10};
+  for (int n = 0 ; n < 5 ; n++)
+  {
+        int temp = *(array + n);               
+        *(array + n) = *(array + (9 - n)); 
+        *(array + (9 - n)) = temp; 
+  }
+   for (int i = 0 ; i < 10 ; i++)
+   {
+        printf ("%d\t" , array[i] );
+   }
+  return 0 ;
+}
